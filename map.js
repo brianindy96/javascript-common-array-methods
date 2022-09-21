ef6e5d4e67999c746914f34371db60847a5798e5
Brian Garton <gartonb@gmail.com>
2022-09-21 13:27:54 +0700

    map.js return Square Root of each element of an array

---
+++ map.js
@@ -0,0 +1,7 @@
+//Return the square root of each numbers in Arrays
+const numbers = [4, 9, 16, 25];
+const roots = numbers.map((x)=>{
+    return Math.sqrt(x);
+})
+
+console.log(roots); // [2,3,4,5];
