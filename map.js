1ac0c2559dad7bda0b90424f540f782e5f5e4c45
Brian Garton <gartonb@gmail.com>
2022-09-21 13:31:09 +0700

    concat a string into an array

--- map.js
+++ map.js
@@ -5,3 +5,14 @@ const roots = numbers.map((x)=>{
 })
 
 console.log(roots); // [2,3,4,5];
+
+// Return an array with 
+const name = ["Brian", "Chanida", "Geoffrey"];
+const full_name = name.map((person)=>{
+    return person + " Garton";
+});
+
+
+console.log(name);  //["Brian", "Chanida", "Geoffrey"]
+console.log(full_name); //["Brian Garton", "Chanida Garton", "Geoffrey"];
+
